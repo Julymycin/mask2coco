20a30bb996c69af6975c7be722b1a0cad3cd93de
Julymycin <cheney.chiu@outlook.com>
2020-08-10 14:23:26 +0800

    update

--- mask_to_coco.py
+++ mask_to_coco.py
@@ -8,17 +8,24 @@ import cv2
 
 import pycococreatortools
 
+import warnings
+warnings.filterwarnings("error", category=UserWarning)
+
+import imageio.plugins.gdal as gdal
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(
         description="transform mask annotation to coco annotation")
     parser.add_argument("--dataset",
                         type=str,
-                        default='MHPv2',
+                        # default='MHPv2',
+                        default='MHPv1',
                         help="name of dataset (CIHP, MHPv2 or VIP)")
     parser.add_argument("--json_save_dir",
                         type=str,
                         default='/home/qiu/Downloads/datasets/LV-MHP-v2',
+                        # default='/home/qiu/Downloads/datasets/LV-MHP-v1',
                         help="path to save coco-style annotation json file")
     parser.add_argument("--use_val",
                         type=bool,
@@ -28,21 +35,25 @@ def get_arguments():
         "--train_img_dir",
         type=str,
         default='/home/qiu/Downloads/datasets/LV-MHP-v2/train/images',
+        # default='/home/qiu/Downloads/datasets/LV-MHP-v1/images',
         help="train image path")
     parser.add_argument(
         "--train_anno_dir",
         type=str,
         default='/home/qiu/Downloads/datasets/LV-MHP-v2/train/parsing_annos',
+        # default='/home/qiu/Downloads/datasets/LV-MHP-v1/annotations',
         help="train human mask path")
     parser.add_argument(
         "--val_img_dir",
         type=str,
         default='/home/qiu/Downloads/datasets/LV-MHP-v2/val/images',
+        # default='',
         help="val image path")
     parser.add_argument(
         "--val_anno_dir",
         type=str,
         default='/home/qiu/Downloads/datasets/LV-MHP-v2/val/parsing_annos',
+        # default='',
         help="val human mask path")
     return parser.parse_args()
 
@@ -75,63 +86,85 @@ def main(args):
         "annotations": []
     }
 
-    mhp2_foot_labels=[20,21,22,23,24,25,26,27,28,29,32,33]
+    # mhp2_foot_labels = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33]
 
-    # image_id = 1
-    # segmentation_id = 1
-    # train_imgs = os.listdir(args.train_img_dir)
-    # for idx, image_name in enumerate(train_imgs):
-    #     if image_name.endswith(('jpg', 'png')):
-    #         # image = Image.open(os.path.join(args.train_img_dir, image_name))
-    #         image=cv2.imread(os.path.join(args.train_img_dir, image_name))
-    #         image_info = pycococreatortools.create_image_info(
-    #             image_id, image_name, image.shape)
-    #         coco_output["images"].append(image_info)
-    #         mask_name_prefix = image_name.split('.')[0]
-    #         temp_i = 0
-    #         # mask_temp = np.ones((image.size[1], image.size[0]),dtype=np.uint8)
-    #         mask_temp = np.ones((image.shape[0],image.shape[1]),dtype=np.uint8)
-    #         while (True):
-    #             mask_path = os.path.join(
-    #                 args.train_anno_dir,
-    #                 mask_name_prefix + '_02_0' + str(temp_i+1) + '.png')
-    #             if os.path.exists(mask_path):
-    #                 mask = np.asarray(Image.open(mask_path))
-    #                 # mask=cv2.imread(mask_path)
-    #                 if len(mask.shape)==3:
-    #                     mask=mask[:,:,0]
-    #                 mask_temp = np.maximum(mask_temp,mask)
-    #                 temp_i += 1
-    #             else:
-    #                 break
-    #         mask_temp=np.where((mask_temp>=20)&(mask_temp<=33)&(mask_temp!=30)&(mask_temp!=31),1,0)
-    #         # cv2.imshow("ss",mask_temp*255.0)
-    #         # cv2.waitKey(-1)
-    #         gt_labels = np.unique(mask_temp)
-    #         for i in range(1, len(gt_labels)):
-    #             category_info = {'id': 1, 'is_crowd': 0}
-    #             binary_mask = np.uint8(mask_temp)
-    #             annotation_info = pycococreatortools.create_annotation_info(
-    #                 segmentation_id,
-    #                 image_id,
-    #                 category_info,
-    #                 binary_mask,
-    #                 binary_mask.shape,
-    #                 tolerance=10)
-    #             if annotation_info is not None:
-    #                 coco_output["annotations"].append(annotation_info)
+    image_id = 1
+    segmentation_id = 1
+    train_imgs = os.listdir(args.train_img_dir)
+    for idx, image_name in enumerate(train_imgs):
+        if image_name.endswith(('jpg', 'png')):
+            # print(image_name+'\n')
+            try:
+                image = Image.open(os.path.join(args.train_img_dir,
+                                                image_name))
+            except:
+                print('corrupt img', image_name)
+            # image=cv2.imread(os.path.join(args.train_img_dir, image_name))
+            image_info = pycococreatortools.create_image_info(
+                image_id, image_name, image.size)
+            coco_output["images"].append(image_info)
+            mask_name_prefix = image_name.split('.')[0]
+            # mask_temp = np.ones((image.size[1], image.size[0]),dtype=np.uint8)
+            mask_temp = np.zeros((image.size[1], image.size[0]), dtype=np.uint8)
 
-    #             segmentation_id += 1
-    #         image_id += 1
-    # if not os.path.exists(args.json_save_dir):
-    #     os.makedirs(args.json_save_dir)
+            count_str = ''
+            ins_count = 0
+            for i in range(1, 100):
+                if i < 10:
+                    count_str = '_0' + str(i)
+                else:
+                    count_str = '_' + str(i)
 
-    # if not args.use_val:
-    #     with open(
-    #             '{}/{}_train.json'.format(args.json_save_dir, args.dataset),
-    #             'w') as output_json_file:
-    #         json.dump(coco_output, output_json_file)
-    
+                if os.path.exists(
+                        os.path.join(args.train_anno_dir, mask_name_prefix +
+                                     count_str + '_01.png')):
+                    ins_count = i
+                    break
+            temp_count = ''
+            for i in range(ins_count):
+                if i < 9:
+                    temp_count = '_0' + str(i + 1)
+                else:
+                    temp_count = '_' + str(i + 1)
+                mask_path = os.path.join(
+                    args.train_anno_dir,
+                    mask_name_prefix + count_str + temp_count + '.png')
+                mask = np.asarray(Image.open(mask_path))
+                # mask=cv2.imread(mask_path)
+                if len(mask.shape) == 3:
+                    mask = mask[:, :, 0]
+                mask_temp = np.where((mask == 9) | (mask == 10), 1, 0)
+                gt_labels = np.unique(mask_temp)
+                for i in range(1, len(gt_labels)):
+                    category_info = {'id': 1, 'is_crowd': 0}
+                    binary_mask = np.uint8(mask_temp)
+                    annotation_info = pycococreatortools.create_annotation_info(
+                        segmentation_id,
+                        image_id,
+                        category_info,
+                        binary_mask,
+                        binary_mask.shape,
+                        tolerance=10)
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id += 1
+
+            # # MHP v2 foot area
+            # mask_temp = np.where((mask_temp >= 20) & (mask_temp <= 33) &
+            #                      (mask_temp != 30) & (mask_temp != 31), 1, 0)
+            # MHP v1 foot area
+            # mask_temp = np.where((mask_temp == 9) | (mask_temp == 10), 1, 0)
+            # cv2.imshow("ss", mask_temp*255.0)
+            # cv2.waitKey(-1)
+            image_id += 1
+    if not os.path.exists(args.json_save_dir):
+        os.makedirs(args.json_save_dir)
+
+    if not args.use_val:
+        with open('{}/{}_train.json'.format(args.json_save_dir, args.dataset),
+                  'w') as output_json_file:
+            json.dump(coco_output, output_json_file)
 
     coco_output_val = {
         "info": INFO,
@@ -147,50 +180,87 @@ def main(args):
     val_imgs = os.listdir(args.val_img_dir)
     for idx, image_name in enumerate(val_imgs):
         if image_name.endswith(('jpg', 'png')):
-            # image = Image.open(os.path.join(args.val_img_dir, image_name))
-            image=cv2.imread(os.path.join(args.val_img_dir, image_name))
+            try:
+                image = Image.open(os.path.join(args.val_img_dir, image_name))
+            except:
+                print('corrupt img', image_name)
+            # print(image_name+'\n')
+            # image=cv2.imread(os.path.join(args.val_img_dir, image_name))
             image_info = pycococreatortools.create_image_info(
-                image_id_val, image_name, image.shape)
+                image_id_val, image_name, image.size)
             coco_output_val["images"].append(image_info)
             mask_name_prefix = image_name.split('.')[0]
-            temp_i = 0
             # mask_temp = np.ones((image.size[1], image.size[0]))
-            mask_temp = np.ones((image.shape[0],image.shape[1]),dtype=np.uint8)
-            while (True):
+            mask_temp = np.zeros((image.size[1], image.size[0]), dtype=np.uint8)
+            count_str = ''
+            ins_count = 0
+            for i in range(1, 100):
+                if i < 10:
+                    count_str = '_0' + str(i)
+                else:
+                    count_str = '_' + str(i)
+
+                if os.path.exists(
+                        os.path.join(args.val_anno_dir, mask_name_prefix +
+                                     count_str + '_01.png')):
+                    ins_count = i
+                    break
+            temp_count = ''
+            for i in range(ins_count):
+                if i < 9:
+                    temp_count = '_0' + str(i + 1)
+                else:
+                    temp_count = '_' + str(i + 1)
                 mask_path = os.path.join(
                     args.val_anno_dir,
-                    mask_name_prefix + '_02_0' + str(temp_i+1) + '.png')
-                if os.path.exists(mask_path):
-                    mask = np.asarray(Image.open(mask_path))
-                    # mask=cv2.imread(mask_path)
-                    if len(mask.shape)==3:
-                        mask=mask[:,:,0]
-                    mask_temp = np.maximum(mask_temp,mask)
-                    temp_i += 1
-                else:
-                    break
-            mask_temp=np.where((mask_temp>=20)&(mask_temp<=33)&(mask_temp!=30)&(mask_temp!=31),1,0)
-            gt_labels = np.unique(mask_temp)
-            for i in range(1, len(gt_labels)):
-                category_info = {'id': 1, 'is_crowd': 0}
-                binary_mask = np.uint8(mask_temp)
-                annotation_info = pycococreatortools.create_annotation_info(
-                    segmentation_id_val,
-                    image_id_val,
-                    category_info,
-                    binary_mask,
-                    binary_mask.shape,
-                    tolerance=10)
-                if annotation_info is not None:
-                    coco_output_val["annotations"].append(annotation_info)
+                    mask_name_prefix + count_str + temp_count + '.png')
+                mask = np.asarray(Image.open(mask_path))
+                # mask=cv2.imread(mask_path)
+                if len(mask.shape) == 3:
+                    mask = mask[:, :, 0]
+                mask_temp = np.where((mask == 9) | (mask == 10), 1, 0)
+                gt_labels = np.unique(mask_temp)
+                for i in range(1, len(gt_labels)):
+                    category_info = {'id': 1, 'is_crowd': 0}
+                    binary_mask = np.uint8(mask_temp)
+                    annotation_info = pycococreatortools.create_annotation_info(
+                        segmentation_id_val,
+                        image_id_val,
+                        category_info,
+                        binary_mask,
+                        binary_mask.shape,
+                        tolerance=10)
+                    if annotation_info is not None:
+                        coco_output_val["annotations"].append(annotation_info)
 
-                segmentation_id_val += 1
+                    segmentation_id_val += 1
+
+            # # MHP v2 foot area
+            # mask_temp = np.where((mask_temp >= 20) & (mask_temp <= 33) &
+            #                      (mask_temp != 30) & (mask_temp != 31), 1, 0)
+            # MHP v1 foot area
             image_id_val += 1
 
-    with open('{}/{}_val.json'.format(args.json_save_dir, args.dataset), 'w') as output_json_file_val:
+    with open('{}/{}_val.json'.format(args.json_save_dir, args.dataset),
+              'w') as output_json_file_val:
         json.dump(coco_output_val, output_json_file_val)
+
+
+def test(args):
+    train_imgs = os.listdir(args.train_img_dir)
+    for idx, image_name in enumerate(train_imgs):
+        if image_name.endswith(('jpg', 'png')):
+            image = Image.open(os.path.join(args.train_img_dir, image_name))
+    val_imgs = os.listdir(args.val_img_dir)
+    for idx, image_name in enumerate(val_imgs):
+        if image_name.endswith(('jpg', 'png')):
+            try:
+                image = Image.open(os.path.join(args.val_img_dir, image_name))
+            except:
+                print('corrupt img', image_name)
 
 
 if __name__ == "__main__":
     args = get_arguments()
     main(args)
+    # test(args)
